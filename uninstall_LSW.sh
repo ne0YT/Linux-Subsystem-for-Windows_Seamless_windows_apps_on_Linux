b5e586bd45319720ea75647cfe417daff8652bae
ne0YT <97364807+ne0YT@users.noreply.github.com>
2022-02-13 13:56:58 +0100

    Add uninstall_LSW.sh

---
+++ uninstall_LSW.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# only root can run this script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# remove all Files
+echo "This will remove all files from LSW"
+echo "It will NOT delete or modify your Windows-VM"
+rm -f /usr/bin/windows.sh
+rm -f  /usr/share/applications/Windows.desktop
+rm -f /usr/bin/savewindows.sh
+rm -f /usr/share/applications/SaveWindows.desktop
